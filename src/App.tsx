b6dbce5cc541b56d389fa5316ee880e92202409c
Andre Sampaio <andremicael@gmail.com>
2024-06-17 19:27:47 -0400

    Created: Components Folders

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="container">
+     <h1> Novo projeto</h1>
+     <button type="button" className="btn btn-outline-success">Success</button>
+     <button type="button" className="btn btn-outline-primary">Primary</button>
+     </div>
+    </>
+  )
+}
+
+export default App
